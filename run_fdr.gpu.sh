07bd7c1ec9b57bb7509223aa0a418ad0bbeb597f
Leo <leo@xl8.ai>
2023-07-27 22:35:04 +0900

    3

--- run_fdr.gpu.sh
+++ run_fdr.gpu.sh
@@ -2,6 +2,9 @@
 # docker run --rm -it -p 10002:10002 -e BATCH_SIZE=6 -e REC_BATCH_SIZE=6 -e GPU_ID=0 \
 #   -e DEBUG=0 -e OLD=0 -e PROFILE=0 -e EMPTY=0 --gpus all leo/face-detection-recognition-cuda --gpu_id=2 --port=10002 --input_size=640
 
+# docker run --rm -d -p 10002:10002 -e BATCH_SIZE=6 -e REC_BATCH_SIZE=6 --gpus all leo/face-detection-recognition-cuda --gpu_id=0 --port=10002 --input_size=160
+# docker run --rm -d -p 10003:10002 -e BATCH_SIZE=6 -e REC_BATCH_SIZE=6 --gpus all leo/face-detection-recognition-cuda --gpu_id=1 --port=10003 --input_size=160
+
 docker run --rm -d -p 10002:10002 -e BATCH_SIZE=6 -e REC_BATCH_SIZE=6 -e DEBUG=0 -e OLD=0 -e PROFILE=0 -e EMPTY=0 --gpus all leo/face-detection-recognition-cuda --gpu_id=0 --port=10002 --input_size=640
 docker run --rm -d -p 10003:10002 -e BATCH_SIZE=6 -e REC_BATCH_SIZE=6 -e DEBUG=0 -e OLD=0 -e PROFILE=0 -e EMPTY=0 --gpus all leo/face-detection-recognition-cuda --gpu_id=1 --port=10002 --input_size=640
 docker run --rm -d -p 10004:10002 -e BATCH_SIZE=6 -e REC_BATCH_SIZE=6 -e DEBUG=0 -e OLD=0 -e PROFILE=0 -e EMPTY=0 --gpus all leo/face-detection-recognition-cuda --gpu_id=2 --port=10002 --input_size=640
